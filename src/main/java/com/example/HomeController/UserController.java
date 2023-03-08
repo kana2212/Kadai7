16931b33f905cd858562f1f7f3971b294a927a2e
kana2212 <cikiruki@yahoo.co.jp>
2023-03-08 22:02:28 +0900

    rest APIを実装する

--- src/main/java/com/example/HomeController/UserController.java
+++ src/main/java/com/example/HomeController/UserController.java
@@ -1,13 +1,7 @@
 package com.example.HomeController;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.net.URI;
@@ -20,7 +14,6 @@ public class UserController {
 
     @GetMapping("/names")
     public List<String> getName() {
-
         return List.of("koyama", "tanaka");
     }
 
@@ -36,14 +29,14 @@ public class UserController {
     }
 
     @PatchMapping("/names/{id}")
-    //更新処理は省略
     public ResponseEntity<Map<String, String>> update(@PathVariable("id") int id, @RequestBody UpdateForm updateForm) {
+        //更新処理は省略
         return ResponseEntity.ok(Map.of("message", "name successfully updated"));
     }
 
     @DeleteMapping("/names/{id}")
-    //削除処理は省略
     public ResponseEntity<Map<String, String>> delete(@PathVariable("id") int id) {
+        //削除処理は省略
         return ResponseEntity.ok(Map.of("message", "id successfully deleted"));
     }
 }
